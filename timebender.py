554d71735111ff5eb7d0a6daf6d28bd4b2a5e41d
Mario Staykov <mario.staykov@gmail.com>
2018-04-24 16:49:34 +0100

    Everything is seconds, except when presenting. Functionality completed, can be enabled.

--- timebender.py
+++ timebender.py
@@ -29,7 +29,8 @@ def getRemainingTime(content):
     returns how much needs logging up to 7.5 hours
     '''
     today = dt.datetime.now().strftime("%Y-%m-%d")
-    totalTimeLoggedToday = 0
+    totalSecondsLoggedToday = 0
+    totalSecondsToWork = 7.5 * 60 * 60
     for issue in content['issues']:
         # Get the worklogs for the current issue
         allWorklogs = sesh.get(api_url + "/issue/" + issue["key"] + "/worklog").json()
@@ -38,9 +39,18 @@ def getRemainingTime(content):
             timeStarted = worklog.get("created")
             if timeStarted[0:10] == today:
                 worklogTimeSpent = worklog.get("timeSpentSeconds")
-                totalTimeLoggedToday += worklogTimeSpent
-                print("-=- A worklog was found for today's worked on ticket {}; adding time of {} to the total.".format(issue["key"],worklogTimeSpent))
-    return(totalTimeLoggedToday)
+                totalSecondsLoggedToday += worklogTimeSpent
+                print("-=- A worklog was found for today's worked on ticket {}; adding {} seconds to the total.".format(issue["key"],worklogTimeSpent))
+
+    # Now that the time logged today is obtained, calculate how much time is left to distribute
+    remainingSecondsToLog = totalSecondsToWork - totalSecondsLoggedToday
+    print("-=- Total time found logged today: {} hours.".format(presentNicelyInHours(totalSecondsLoggedToday)))
+    return(remainingSecondsToLog)
+
+def presentNicelyInHours(seconds):
+    inHours = seconds / 60 / 60
+    inHours_delimited = round(inHours, 2)
+    return(str(inHours_delimited))
 
 def addWorklog(targetIssue_key, time_to_pool):
     payload = {
@@ -51,7 +61,7 @@ def addWorklog(targetIssue_key, time_to_pool):
     print(api_url + "/issue/" + targetIssue_key + "/worklog")
     postOutcome = sesh.post(api_url + "/issue/" + targetIssue_key + "/worklog", json=payload)
     if postOutcome.status_code == 201:
-        print("-=- Time logged well.")
+        print("-=- Time successfully balanced up to 7.5 hours.")
     else:
         print("-!- Problem with error code {}".format(postOutcome.status_code))
 
@@ -62,10 +72,11 @@ if __name__ == "__main__":
         query_todaysLoggedTickets = "worklogAuthor = currentUser() AND worklogDate = endOfDay() "
         content = getContentForQuery(query_todaysLoggedTickets)
 
-        print("-=- Calculating time spent across all tickets today")
-        timeToBalance = getRemainingTime(content)
-        print(timeToBalance)
+        print("-=- Calculating time spent across all tickets today...")
+        remainingSecondsToLog = getRemainingTime(content)
 
+        print("-=- T i m e to choose which ticket to pool the remaining {} hours into...\n".format(presentNicelyInHours(remainingSecondsToLog)))
         targetIssue = getChoiceAfterPresenting(content)
-        print("-=- Logging time into the chosen ticket {}...".format(targetIssue["key"]))
-        # addWorklog(targetIssue["key"], timeToBalance)
+
+        print("-=- Logging {} hours into the chosen ticket {}...".format(presentNicelyInHours(remainingSecondsToLog), targetIssue["key"]))
+        # addWorklog(targetIssue["key"], remainingSecondsToLog)
